b770a4bcbbb790bf716e168f8e794656cecfbcdc
Adrian Sowandi <adrian.sowandi@stonybrook.edu>
2021-04-07 17:23:40 +0900

    testing3 node js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
-
 const app = express();
-const port = process.env.PORT || 3000 || 80;
+const port = process.env.PORT || 80 || 3000;
 
-app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.listen(port, (req, res) => {
-  res.send(`Listening on port ${port}`);
+app.listen(port, () => {
+  console.log(`Example app listening at ${port}`);
 });
